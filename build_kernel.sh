9f006f3d2b7d70eb609988bff313e695ebdf4633
Felipe C. Juliano <46896206+FelipeJuliano@users.noreply.github.com>
2023-10-11 16:25:47 -0300

    build script

---
+++ build_kernel.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export ARCH=arm64
+
+# GCC from https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9
+BUILD_CROSS_COMPILE=/home/felipe/Downloads/toolchain/gcc/bin/aarch64-linux-android-
+
+# SD-Clang 10 from https://github.com/xiangfeidexiaohuo/Snapdragon-LLVM
+KERNEL_LLVM_BIN=/home/felipe/Downloads/toolchain/sdclang/bin/clang
+
+CLANG_TRIPLE=aarch64-linux-gnu-
+
+make -j$(nproc --all) ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/lineage-m52xq_defconfig
+make -j$(nproc --all) ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+
